4e48a3136eba5cf4c385eff73bdcf109a2d2cfa8
Riyad <pycoder028@gmail.com>
2024-09-13 03:08:11 +0600

    Dashboard Routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -27,5 +28,13 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+/* admin group rotues */
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::controller(DashboardController::class)->group(function () {
+        Route::get('admin/dashboard','Index');
+    });
+});
+
 
 require __DIR__.'/auth.php';
